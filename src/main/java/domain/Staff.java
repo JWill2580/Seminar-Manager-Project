a3f9484b87ecf4e8405bb724385f3087704ec3fe
lucyberes <123>
2020-04-07 18:30:20 +1200

    Added the staff domain class

---
+++ src/main/java/domain/Staff.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author lucyberes
+ */
+public class Staff {
+    
+    private String staffID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String surname;
+    private String bio;
+    private String email;
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" + "staffID=" + staffID + ", username=" + username + '}';
+    }
+    
+    
+}
