effc531bff9a8a0d69479a6d725c87797d15c765
Ain Khairul Noramin <khaai094@student.otago.ac.nz>
2020-04-17 13:39:50 +1200

    created staff and seminar tables and linked them through PK and FK, along with 2 inserts of data for testing

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,33 @@
  * Created: 9/04/2020
  */
 
+create table Staff (
+
+StaffID INT,
+Username varchar(20) NOT NULL UNIQUE,
+Password varchar(100) NOT NULL,
+FirstName varchar(100) NOT NULL,
+LastName varchar(100) NOT NULL,
+Bio TEXT,
+StaffEmail varchar(50) NOT NULL,
+CONSTRAINT Staff_PK PRIMARY KEY (StaffID)
+);
+
+CREATE TABLE Seminar(
+
+SeminarID INT,
+Topic varchar(255) NOT NULL,
+Title varchar(255) NOT NULL,
+Abstract TEXT NOT NULL,
+Location Varchar(255) NOT NULL,
+Modality Varchar(255) NOT NULL,
+Day_Date DATE NOT NULL,
+StaffID int,
+CONSTRAINT Seminar_PK PRIMARY KEY (SeminarID),
+CONSTRAINT StaffSeminar_FK FOREIGN KEY (StaffID) REFERENCES Staff(StaffID)
+
+);
+
+INSERT Into Staff (StaffID, Username, Password, FirstName, LastName, Bio, StaffEmail) VALUES (1,'oolong1234','Meeps445','John','Ong','This is my talk', 'oolong1234@gmail.com');
+
+INSERT INTO Seminar (SeminarID, Topic, Title, Abstract, Location, Modality, Day_Date, StaffID) VALUES (1, 'Tea', 'Types of Tea', 'This is where it started', 'OBS2.57', 'Spreading joy', '17-APR-2020', 1);
